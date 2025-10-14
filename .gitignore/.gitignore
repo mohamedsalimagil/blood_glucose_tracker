984ced13416b25b35dae29d42129cda42c29cfad
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-10-14 21:58:56 +0300

    .gitignore

---
+++ .gitignore/.gitignore
@@ -0,0 +1,15 @@
+# Ignore SQLite database
+data/*.db
+
+# Ignore Python cache files
+__pycache__/
+*.pyc
+*.pyo
+*.pyd
+
+# VS Code settings
+.vscode/
+
+# Environment folders
+env/
+venv/
--- database.py
+++ database.py
@@ -1,15 +1,1 @@
-# Ignore SQLite database
-data/*.db
 
-# Ignore Python cache files
-__pycache__/
-*.pyc
-*.pyo
-*.pyd
-
-# VS Code settings
-.vscode/
-
-# Environment folders
-env/
-venv/
