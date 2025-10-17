623918631a06ef3c06ce9185c46c2d4d6b3cf755
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-10-17 20:06:45 +0300

    Add comments  for clarity of the file

---
+++ main.py
@@ -0,0 +1,3 @@
+# This file will act as the main entry point in the future.
+# For now, the CLI runs the app, but main.py is kept for possible expansion
+# (e.g., GUI, web version, or different app modes).
